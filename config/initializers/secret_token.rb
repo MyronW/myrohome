1d675016d86286125eb562a6abbe9a98cc577f19
Myron Wilson <myronw@gmail.com>
2014-06-10 23:39:17 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyroHome::Application.config.secret_key_base = '3a8092a884a604934e8cfc9ef2c6557ade2168d24761feab025e5c6918367ee9743da924ef54c778f2c15989959315ed081999fd243b74247742517735c33082'
